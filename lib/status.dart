02982c430333c0346f8c45ab819723518edddc21
Victor Emanuel <victor.emanuel@esig.com.br>
2021-07-30 10:06:07 -0300

    Revert "removes flutter dependencies"
    This reverts commit ca239668d1aff6c332f73b6b18de8d5f3be5d493.

--- lib/status.dart
+++ lib/status.dart
@@ -1,10 +1,9 @@
 enum Status {
+  ERRO,
+  CONCLUIDO,
   VAZIO,
   AGUARDANDO,
-  CONCLUIDO,
-  ERRO,
 }
-
 extension StatusExt on Status {
   static String msg = '';
   set mensagem(String novaMsg) => msg = novaMsg;
