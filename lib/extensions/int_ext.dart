293600287741bd62a2137dda02f4378a78b07a43
mobile-esig <73959322+mobile-esig@users.noreply.github.com>
2024-05-27 13:29:09 -0300

    Corrige pipeline

--- lib/extensions/int_ext.dart
+++ lib/extensions/int_ext.dart
@@ -1,6 +1,6 @@
 extension IntExtension on int {
   /// FormDateTime.fromMillisecondsSinceEpoch(a)ta valores para terem pelo menos 2 dígitos.
-  String get pad => this >= 0 && this < 10 ? '0' + toString() : toString();
+  String get pad => this >= 0 && this < 10 ? '0${toString()}' : toString();
 
   /// Converte inteiros no formato timestamp para [DateTime]
   DateTime get toDateTime => DateTime.fromMillisecondsSinceEpoch(this);
